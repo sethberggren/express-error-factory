db89484ac313cb1cfa9732c3dd2a430dc70faedf
Seth Berggren <seth.berggren@gmail.com>
2022-08-09 00:49:05 -0500

    Add JSDoc documentation to functions and classes.

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,18 @@ type ErrorResponses<ErrorTypes extends StringOnlyObject> = {
   };
 };
 
+/**
+ * Custom error class to be used by the CustomErrorThrower class.
+ * @extends Error
+ */
 export class ExpressCustomError extends Error {
+  /**
+   * Custom error class.  Will rarely be called itself, but is called by the CustomErrorThrower class.
+   *
+   * @param type Error type to be returned in response
+   * @param statusCode Error status code to be returned in response
+   * @param message Error message to be returned in response
+   */
   type: string;
   statusCode: number;
   message: string;
@@ -34,22 +45,48 @@ export class ExpressCustomError extends Error {
   }
 }
 
+/**
+ * Class to generate and throw custom errors.
+ */
 export default class CustomErrorThrower<
   UserErrorResponses extends ErrorResponses<StringOnlyObject>
 > {
   private errorResponses: UserErrorResponses;
 
+  /**
+   *
+   * @param errorResponses Object containing all error types and error subtypes with their corresponding messages as nested objects.
+   */
   constructor(errorResponses: UserErrorResponses) {
     this.errorResponses = errorResponses;
   }
 
+  /**
+   *
+   * @param type Error type
+   * @returns Function that takes in error subtype and returns the error type, status code, and message.
+   */
   public getErrorResponse =
     <Type extends keyof UserErrorResponses>(type: Type) =>
+    /**
+     *
+     * @param subtype Error subtype
+     * @returns Object including error type, status code, and message.
+     */
     <Subtype extends keyof UserErrorResponses[Type]>(subtype: Subtype) =>
       this.errorResponses[type][subtype];
 
+  /**
+   *
+   * @param type Error type
+   * @returns Function that takes in error subtype and throws new error.
+   */
   public throwCustomError =
     <Type extends keyof UserErrorResponses>(type: Type) =>
+    /**
+     *
+     * @param subtype Error subtype
+     */
     <Subtype extends keyof UserErrorResponses[Type]>(subtype: Subtype) => {
       const errorResponse = this.getErrorResponse(type)(subtype);
 
